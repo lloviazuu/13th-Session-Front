b1f2501954cc9386f2595b84e32ea7ea41b0151d
3olly <cy.kim@ewhain.net>
2025-05-08 07:51:26 +0900

    fix: React_1 세션 자료 수정

--- React_1/src/App.js
+++ React_1/src/App.js
@@ -1,15 +1,9 @@
 import { useState } from "react";
-import Name from "./component/Name";
-import Input from "./component/Input";
-import Modal from "./component/Modal";
+import Name from "./components/Name";
+import Input from "./components/Input";
+import Modal from "./components/Modal";
 
 function App() {
-  const lionList = [
-    { name: "아기사자", nth: "11", isFE: true },
-    { name: "아기사자", nth: "12", isFE: false },
-    { name: "아기사자", nth: "13", isFE: true },
-  ];
-  //데이터셋은 자유롭게 추가 및 변경이 가능합니다! 다양한 데이터를 만들어 띄워보세요 :)
   return (
     <>
       <Name name="아기사자" nth="13" isFE="true" />
